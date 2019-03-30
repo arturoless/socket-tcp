de5bdd06900ad1a8de140e136ab29d756ded131d
arturoless <43190646+arturoless@users.noreply.github.com>
2019-03-30 08:08:12 -0600

    2

---
+++ s2.js
@@ -0,0 +1,45 @@
+const os = require('os')
+const net = require('net')
+const Server = require('socket.io');
+const PORTIO  = 4040;
+const server = require('http').Server();
+server.listen(PORTIO); 
+const io = Server(server);
+
+var interfaces = os.networkInterfaces()
+var wifi = interfaces['eth0']
+if(wifi==null){
+    wifi = interfaces['Wi-Fi']
+    var ipv4 = wifi[1]
+}
+else{
+    var ipv4 = wifi[0]
+}
+var HOST = ipv4['address']
+var PORT = process.env.PORT || 5500
+var serverTCP = net.createServer(function(socket){
+    console.log('-----------------------Usuario nuevo-------------------------------')
+    io.on('connection',function(so){
+
+        socket.on('data', (data)=>{
+            so.on('riego', (data)=>{
+                socket.write(data)
+            })
+            so.on('eliminar', (data)=>{
+                socket.write(data)
+            })
+            
+        })
+        socket.on('close', ()=>{
+            console.log('Usuario desconectado')
+        })
+    })
+    
+})
+
+serverTCP.listen(PORT,() => {
+    console.log('-------------------------------------------------------------------')
+    console.log('Conectado en: '+HOST+':' + PORT);
+    console.log('-------------------------------------------------------------------')
+  });
+
