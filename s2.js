32be71a5c041bba2777e81e857fdc9c4f6fb384e
arturoless <43190646+arturoless@users.noreply.github.com>
2019-03-30 09:38:05 -0600

    Update s2.js

--- s2.js
+++ s2.js
@@ -1,43 +1,16 @@
-const os = require('os')
-const net = require('net')
 const Server = require('socket.io');
 const PORTIO  = 4040;
 const server = require('http').Server();
-server.listen(PORTIO); 
+server.listen(PORTIO);
 const io = Server(server);
-
-var interfaces = os.networkInterfaces()
-var wifi = interfaces['eth0']
-if(wifi==null){
-    wifi = interfaces['Wi-Fi']
-    var ipv4 = wifi[1]
-}
-else{
-    var ipv4 = wifi[0]
-}
-var HOST = ipv4['address']
-var PORT = process.env.PORT || 5500
-var serverTCP = net.createServer(function(socket){
-    console.log('-----------------------Usuario nuevo-------------------------------')
-    io.on('connection',function(so){
-        so.on('riego', (datos)=>{
-            socket.write(datos)
-        })
-        so.on('eliminar', (datos)=>{
-            console.log(datos)
-            socket.write(datos)
-            
-        })
-        socket.on('close', ()=>{
-            console.log('Usuario desconectado')
-        })
+io.on('connection',function(so){
+    so.on('riego', (datos)=>{
+        console.log(datos)
+        so.broadcast.emit('riegopi', datos);
     })
-    
+    so.on('eliminar', (datos)=>{
+        console.log(datos)
+        so.broadcast.emit('eliminarpi', datos);
+
+    })
 })
-
-serverTCP.listen(PORT,() => {
-    console.log('-------------------------------------------------------------------')
-    console.log('Conectado en: '+HOST+':' + PORT);
-    console.log('-------------------------------------------------------------------')
-  });
-
